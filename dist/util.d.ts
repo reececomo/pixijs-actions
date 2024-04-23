711b8f31fa2f1e04f4af7ba05b0005fa385d4278
Reece Como <reece.como@gmail.com>
2024-04-24 02:27:48 +1000

    don't commit dist

--- dist/util.d.ts
+++
@@ -1,5 +0,0 @@
-import * as PIXI from 'pixi.js';
-/** @returns Whether the target is paused, including via any ancestors. */
-export declare function getIsPaused(target: PIXI.DisplayObject): boolean;
-/** @returns The targets action speed, after factoring in any ancestors. */
-export declare function getSpeed(target: PIXI.DisplayObject): number;
