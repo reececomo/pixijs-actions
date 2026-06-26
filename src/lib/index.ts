2d82804d16ebff6cfb1419d17a3664198a0f2c75
Wendley Paulo de França <wendley.franca@virtus.ufcg.edu.br>
2026-06-26 09:01:42 -0300

    chore: add ActionTicker export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,11 @@
 // apply mixins
 import { Action } from './ActionClass';
+import { ActionTicker } from "./ActionTicker";
 
 import './Action.mixin.initializers';
 import './Action.mixin.defaults';
 import './Action.mixin.tick';
 
 // exports
-export { Action };
+export { Action, ActionTicker };
 export { Timing, type TimingFunction, type TimingKey } from './Timing';
